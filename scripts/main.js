a32fb961303acd215eee0a01eae84f9b053d2bfe
Min Ji Seo <seominji92@gmail.com>
2023-02-22 01:10:59 -0800

    Add login authentication and format main page by Min Ji Seo

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+function insertName() {
+  firebase.auth().onAuthStateChanged(user => {
+      // Check if a user is signed in:
+      if (user) {
+          // Do something for the currently logged-in user here: 
+          console.log(user.uid); //print the uid in the browser console
+          console.log(user.displayName);  //print the user name in the browser console
+          user_Name = user.displayName;
+
+          //method #1:  insert with html only
+          //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+          //method #2:  insert using jquery
+          $("#name-goes-here").text(user_Name); //using jquery
+
+      } else {
+          // No user is signed in.
+      }
+  });
+}
+insertName(); //run the function
