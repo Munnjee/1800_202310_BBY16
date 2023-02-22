e5388f42370d9b38e3776b7b1aa0dd5a266487f5
Min Ji Seo <seominji92@gmail.com>
2023-02-21 19:14:44 -0800

    Firebase set up and navbar skeleton link by Min Ji Seo

---
+++ scripts/skeleton.js
@@ -0,0 +1,8 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton(){
+  console.log($('#navbarPlaceholder').load('./text/nav.html'));
+}
+loadSkeleton();  //invoke the function
