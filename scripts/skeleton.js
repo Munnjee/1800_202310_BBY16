8ca1c0038d4d1b99fe9df97e26b1fef0fa5668fb
Min Ji Seo <seominji92@gmail.com>
2023-03-04 14:31:55 -0800

    create logout button (html, CSS, JS) by MJS

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -13,3 +13,17 @@ function loadSkeleton() {
   });
 }
 loadSkeleton(); //invoke the function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
+}
+
+
