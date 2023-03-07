7058dd669220872fd6e7a7b43ec008bcfa87d46f
Min Ji Seo <seominji92@gmail.com>
2023-03-07 03:26:09 -0800

    Edit js function to display name for Profile edit page by MJS

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,3 @@
-function insertName() {
-  firebase.auth().onAuthStateChanged((user) => {
-    // Check if a user is signed in:
-    if (user) {
-      console.log(user.uid); //print the uid in the browser console
-      console.log(user.displayName); //print the user name in the browser console
-      user_Name = user.displayName;
-
-      //insert name using jquery
-      $("#name-goes-here").text(user_Name); //using jquery
-    } else {
-      // No user is signed in.
-    }
-  });
-}
-insertName(); //run the function
-
 // toggle accordion
 var accordion = document.querySelector(".accordion");
 accordion.addEventListener("click", function () {
--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,20 @@
+function insertName() {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if a user is signed in:
+    if (user) {
+      console.log(user.uid); //print the uid in the browser console
+      console.log(user.displayName); //print the user name in the browser console
+      user_Name = user.displayName;
+
+      //insert name using jquery
+      $("#name-goes-here").text(user_Name); //using jquery
+    } else {
+      // No user is signed in.
+    }
+  });
+}
+insertName(); //run the function
+
 function populateUserInfo() {
   firebase.auth().onAuthStateChanged(user => {
       // Check if user is signed in:
