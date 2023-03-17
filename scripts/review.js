d93528ecdaf014403a8b701c65b9c01614ad5073
parinrava <pravanbakhsh@my.bcit.ca>
2023-03-16 18:29:50 -0700

    edited review.html and review.js

--- scripts/review.js
+++ scripts/review.js
@@ -1,21 +1,7 @@
 
-var gigDocID = localStorage.getItem("gigDocID");    //visible to all functions on this page
-
-function getGigName(id) {
-    db.collection("giglisting")
-      .doc(id)
-      .get()
-      .then((thisGig) => {
-        var GigName = thisGig.data().jobTitle;
-        document.getElementById("gigName").innerHTML = GigName;
-          });
-}
-
-getGigName(gigDocID);
-
 function populateReviews() {
-    let hikeCardTemplate = document.getElementById("reviewCardTemplate");
-    let hikeCardGroup = document.getElementById("reviewCardGroup");
+    let gigCardTemplate = document.getElementById("reviewCardTemplate");
+    let gigCardGroup = document.getElementById("reviewCardGroup");
     var GiglistingID = localStorage.getItem("Giglisting");
    // let params = new URL(window.location.href) //get the url from the searbar
    // let hikeID = params.searchParams.get("docID")
@@ -23,27 +9,27 @@ function populateReviews() {
     // doublecheck: is your collection called "Reviews" or "reviews"?
     db.collection("reviews").where( "GiglistDocID", "==", GiglistingID).get()
         .then(allReviews => {
-            reviews=allReviews.docs;
+            reviews = allReviews.docs;
             console.log(reviews);
             reviews.forEach(doc => {
                 var title = doc.data().title; //gets the name field
                 var level = doc.data().level; //gets the unique ID field
                 var season = doc.data().season;
                 var description = doc.data().description; //gets the length field
-                var flooded = doc.data().flooded;
-                var scrambled = doc.data().scrambled;
+                var redoGig = doc.data().redoGig;
+                var workWithEmployerAgain = doc.data().workWithEmployerAgain;
                 var time = doc.data().timestamp.toDate();
                 console.log(time)
 
-                let reviewCard = hikeCardTemplate.content.cloneNode(true);
+                let reviewCard = gigCardTemplate.content.cloneNode(true);
                 reviewCard.querySelector('.title').innerHTML = title;     //equiv getElementByClassName
                 reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();    //equiv getElementByClassName
                 reviewCard.querySelector('.level').innerHTML = `level: ${level}`;
                 reviewCard.querySelector('.season').innerHTML = `season: ${season}`;
-                reviewCard.querySelector('.scrambled').innerHTML = `scrambled: ${scrambled}`;  //equiv getElementByClassName
-                reviewCard.querySelector('.flooded').innerHTML = `flooded: ${flooded}`;  //equiv getElementByClassName
+                reviewCard.querySelector('.workWithEmployerAgain').innerHTML = `workWithEmployerAgain: ${workWithEmployerAgain}`;  //equiv getElementByClassName
+                reviewCard.querySelector('.redoGig').innerHTML = `redoGig: ${redoGig}`;  //equiv getElementByClassName
                 reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
-                hikeCardGroup.appendChild(reviewCard);
+                gigCardGroup.appendChild(reviewCard);
             })
         })
 }
