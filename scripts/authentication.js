7e643f72a8c4665236d65bc824cbe71dc411ffa3
Min Ji Seo <seominji92@gmail.com>
2023-03-23 03:49:07 -0700

    finalize css format for main.html by mjs

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -30,14 +30,8 @@ var uiConfig = {
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
   signInSuccessUrl: "./html/main.html",
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  signInOptions: [   
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>',
