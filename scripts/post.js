fdf1397b4933c5316cf5c57471c99c33ec1c0022
parinrava <pravanbakhsh@my.bcit.ca>
2023-03-23 08:30:21 -0700

    initialized fire storage to save the pictures from post form as a url

--- scripts/post.js
+++ scripts/post.js
@@ -1,17 +1,17 @@
 var ImageFile;
 
-        function listenFileSelect() {
-            // listen for file selection
-            var fileInput = document.getElementById("fileinput"); // pointer #1
-            const image = document.getElementById("file"); // pointer #2
+function listenFileSelect() {
+  // listen for file selection
+  var fileInput = document.getElementById("fileinput"); // pointer #1
+  const image = document.getElementById("file"); // pointer #2
 
-            fileInput.addEventListener('change', function (e) {
-                ImageFile = e.target.files[0];
-                var blob = URL.createObjectURL(ImageFile);
-                image.src = blob; // display this image
-            })
-        }
-        listenFileSelect();
+  fileInput.addEventListener('change', function (e) {
+    ImageFile = e.target.files[0];
+    var blob = URL.createObjectURL(ImageFile);
+    image.src = blob; // display this image
+  })
+}
+listenFileSelect();
 
 
 //Saves Data to Firestore for Gig Posts
@@ -40,8 +40,7 @@ postForm.addEventListener("submit", (event) => {
         flexDate: postForm.flexibleDate.value,
         time: postForm.time.value,
         flexTime: postForm.flexibleTime.value,
-        file: postForm.file.value,
-      }).then(doc=> {
+      }).then(doc => {
         alert("Your Gig Post is Successful!");
         console.log("Gig Post Document Added!");
         console.log(doc.id);
@@ -56,37 +55,61 @@ postForm.addEventListener("submit", (event) => {
     }
   });
 
+});
 
+
+// function savePost() {
+//   alert ("SAVE POST is triggered");
+//   firebase.auth().onAuthStateChanged(function (user) {
+//       if (user) {
+//           // User is signed in.
+//           // Do something for the user here. 
+//          // var desc = document.getElementById("description").value;
+
+
+
+//           db.collection("giglisting").add({
+//              // owner: user.uid,
+//              // description: desc,
+
+
+//               last_updated: firebase.firestore.FieldValue
+//                   .serverTimestamp() //current system time
+//           }).then(doc => {
+//               console.log("Post document added!");
+//               console.log(doc.id);
+//               uploadPic(doc.id);
+//           })
+//       } else {
+//           // No user is signed in.
+//           console.log("Error, no user signed in");
+//       }
+//   });
+// }
 
 
 //function that pull url from the website
-
 function uploadPic(giglistingDocID) {
   console.log("inside uploadPic " + giglistingDocID);
   var storageRef = storage.ref("images/" + giglistingDocID + ".jpg");
 
   storageRef.put(ImageFile)
-      .then(function () {
-          console.log('Uploaded to Cloud Storage.');
-          storageRef.getDownloadURL()
-              .then(function (url) { // Get URL of the uploaded file
-                  console.log("Got the download URL.");
-                  db.collection("giglisting").doc(giglistingDocID).update({
-                          "image": url // Save the URL into users collection
-                      })
-                      .then(function () {
-                          console.log('Added pic URL to Firestore.');
-                      })
-              })
-      })
-      .catch((error) => {
-          console.log("error uploading to cloud storage");
-      })
+    .then(function () {
+      console.log('Uploaded to Cloud Storage.');
+      storageRef.getDownloadURL()
+        .then(function (url) { // Get URL of the uploaded file
+          console.log("Got the download URL.");
+          db.collection("giglisting").doc(giglistingDocID).update({
+            "image": url // Save the URL into users collection
+          })
+            .then(function () {
+              console.log('Added pic URL to Firestore.');
+            })
+        })
+    })
+    .catch((error) => {
+      console.log("error uploading to cloud storage");
+    })
 }
-
-
-});
-
-
 
 
