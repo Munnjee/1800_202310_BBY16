f0893f19f6cb5c60788fd11841c417c7b23dcc1c
Min Ji Seo <seominji92@gmail.com>
2023-03-23 03:04:25 -0700

    reorganize html files into folder

--- scripts/gigapplicants.js
+++ scripts/gigapplicants.js
@@ -1,17 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function displayGigDescription(collect) {
     let params = new URL( window.location.href ); //get URL of search bar
     let ID = params.searchParams.get( "docID" ); //get value for key "id"
