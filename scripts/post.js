8f2a7c100446bf6f12ef9cbc70b95f55c6aab50c
Min Ji Seo <seominji92@gmail.com>
2023-04-02 12:51:42 -0700

    minor adjustment to post.js

--- scripts/post.js
+++ scripts/post.js
@@ -5,14 +5,13 @@ function listenFileSelect() {
   var fileInput = document.getElementById("fileinput"); // pointer #1
   const image = document.getElementById("file"); // pointer #2
 
-  fileInput.addEventListener('change', function (e) {
+  fileInput.addEventListener("change", function (e) {
     ImageFile = e.target.files[0];
     var blob = URL.createObjectURL(ImageFile);
     image.src = blob; // display this image
-  })
+  });
 }
 listenFileSelect();
-
 
 //Saves Data to Firestore for Gig Posts
 
@@ -27,30 +26,38 @@ postForm.addEventListener("submit", (event) => {
     if (user) {
       // User Signed In
       // Create a new document in Firestore
-      const compensationValue = parseFloat(postForm.compensation.value.replace(/\$/g, ''));
-      db.collection("giglisting").add({
-        owner: user.uid,
-        postTime: firebase.firestore.FieldValue.serverTimestamp(),
-        jobTitle: postForm.gig_title.value,
-        description: postForm.description.value,
-        compensation: compensationValue,
-        location: postForm.location.value,
-        indooroutdoor: postForm.indooroutdoor.value,
-        date: postForm.date.value,
-        flexDate: postForm.flexibleDate.checked ? postForm.flexibleDate.value : "",
-        time: postForm.time.value,
-        flexTime: postForm.flexibleTime.checked ? postForm.flexibleTime.value : "",
-      }).then(doc => {
-        alert("Your Gig Post is Successful!");
-        console.log("Gig Post Document Added!");
-        console.log(doc.id);
-        uploadPic(doc.id);
-        window.location.href = "main.html";
-      });
+      const compensationValue = parseFloat(
+        postForm.compensation.value.replace(/\$/g, "")
+      );
+      db.collection("giglisting")
+        .add({
+          owner: user.uid,
+          postTime: firebase.firestore.FieldValue.serverTimestamp(),
+          jobTitle: postForm.gig_title.value,
+          description: postForm.description.value,
+          compensation: compensationValue,
+          location: postForm.location.value,
+          indooroutdoor: postForm.indooroutdoor.value,
+          date: postForm.date.value,
+          flexDate: postForm.flexibleDate.checked
+            ? postForm.flexibleDate.value
+            : "",
+          time: postForm.time.value,
+          flexTime: postForm.flexibleTime.checked
+            ? postForm.flexibleTime.value
+            : "",
+        })
+        .then((doc) => {
+          alert("Your Gig Post is Successful!");
+          console.log("Gig Post Document Added!");
+          console.log(doc.id);
+          uploadPic(doc.id);
+          postForm.reset();
+        });
     } else {
       // No User Signed In
-      alert("Please Sign-in to Post a Gig")
-      console.log("Error, No user signed in")
+      alert("Please Sign-in to Post a Gig");
+      console.log("Error, No user signed in");
     }
   });
 });
@@ -60,27 +67,24 @@ function uploadPic(giglistingDocID) {
   console.log("inside uploadPic " + giglistingDocID);
   var storageRef = storage.ref("images/" + giglistingDocID + ".jpg");
 
-  storageRef.put(ImageFile)
+  storageRef
+    .put(ImageFile)
     .then(function () {
-      console.log('Uploaded to Cloud Storage.');
-      storageRef.getDownloadURL()
-        .then(function (url) { // Get URL of the uploaded file
-          console.log("Got the download URL.");
-          db.collection("giglisting").doc(giglistingDocID).update({
-            "image": url // Save the URL into users collection
+      console.log("Uploaded to Cloud Storage.");
+      storageRef.getDownloadURL().then(function (url) {
+        // Get URL of the uploaded file
+        console.log("Got the download URL.");
+        db.collection("giglisting")
+          .doc(giglistingDocID)
+          .update({
+            image: url, // Save the URL into users collection
           })
-            .then(function () {
-              console.log('Added pic URL to Firestore.');
-            })
-        })
+          .then(function () {
+            console.log("Added pic URL to Firestore.");
+          });
+      });
     })
     .catch((error) => {
       console.log("error uploading to cloud storage");
-    })
+    });
 }
-
-
-
-
-
-
